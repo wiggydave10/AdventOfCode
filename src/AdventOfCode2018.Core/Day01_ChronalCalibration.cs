d43ff0f125e47bdf322c71048d202a3b22c8c05d
Matthew Yates <matthewy10@hotmail.com>
2018-12-01 18:52:55 +0000

    Completed Day 1 Part 1.

---
+++ src/AdventOfCode2018.Core/Day01_ChronalCalibration.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace AdventOfCode2018.Core
+{
+    public class Day01_ChronalCalibration
+    {
+        public static int Process_Part1(int startingFrequency, int[] frequencyChanges)
+        {
+            foreach (var frequencyChange in frequencyChanges)
+            {
+                startingFrequency += frequencyChange;
+            }
+
+            return startingFrequency;
+        }
+
+        //public static void Process_Part2()
+        //{
+
+        //}
+    }
+}
---
+++ src/AdventOfCode2018.Tests/Day01_ChronalCalibrationTests.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode2018.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2018.Tests
+{
+    [TestClass]
+    public class Day01_ChronalCalibrationTests
+    {
+        [TestMethod]
+        public void Day01_ChronalCalibrationTests_Part1_Test()
+        {
+            var startingFreq = 0;
+            var frequencyChanges = new[] { +1, -2, +3, +1 };
+            var result = Day01_ChronalCalibration.Process_Part1(startingFreq, frequencyChanges);
+
+            result.ShouldBe(3);
+        }
+
+        [TestMethod]
+        public void Day01_ChronalCalibrationTests_Part1_Answer()
+        {
+            var stringNumbers = File.ReadAllLines(TestResourceService.GetFilePath("Day01.txt"));
+
+            var startingFreq = 0;
+            var frequencyChanges = stringNumbers.Select(int.Parse).ToArray();
+            var result = Day01_ChronalCalibration.Process_Part1(startingFreq, frequencyChanges);
+
+            result.ShouldBe(0);
+        }
+
+        [TestMethod]
+        public void Day01_ChronalCalibrationTests_Part2_Test()
+        {
+            //var startingFreq = 0;
+            //var frequencyChanges = new[] { +1, -2, +3, +1 };
+            //var result = Day01_ChronalCalibration.Process(startingFreq, frequencyChanges);
+
+            //result.ShouldBe(3);
+        }
+
+        [TestMethod]
+        public void Day01_ChronalCalibrationTests_Part2_Answer()
+        {
+            //var stringNumbers = File.ReadAllLines(TestResourceService.GetFilePath("Day01.txt"));
+
+            //var startingFreq = 0;
+            //var frequencyChanges = stringNumbers.Select(int.Parse).ToArray();
+            //var result = Day01_ChronalCalibration.Process(startingFreq, frequencyChanges);
+
+            //result.ShouldBe(0);
+        }
+    }
+}
