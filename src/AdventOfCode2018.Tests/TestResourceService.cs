24e2a487e6aa14ea867fc38e3d7fa9fbd9c4b12a
Matthew Yates <matthewy10@hotmail.com>
2018-12-01 18:16:02 +0000

    Added solution and projects.

---
+++ src/AdventOfCode2018.Tests/TestResourceService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2018.Tests
+{
+    public static class TestResourceService
+    {
+        private const string resourcePath = "..\\..\\..\\..\\Files";
+
+        public static string GetFilePath(string filename)
+        {
+            var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            return Path.Combine(basePath, resourcePath, filename);
+        }
+    }
+}
