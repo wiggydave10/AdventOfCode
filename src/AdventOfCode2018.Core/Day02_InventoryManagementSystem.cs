7fd6363e28078a5bea43628c1d950566f8491942
Matthew Yates <matthewy10@hotmail.com>
2018-12-02 17:51:25 +0000

    Completed Day 2 Part 2

--- src/AdventOfCode2018.Core/Day02_InventoryManagementSystem.cs
+++ src/AdventOfCode2018.Core/Day02_InventoryManagementSystem.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2018.Core
 {
     public static class Day02_InventoryManagementSystem
     {
-        public static int Process(string[] ids)
+        public static int Process_Part1(string[] ids)
         {
             var twos = 0;
             var threes = 0;
@@ -20,5 +21,67 @@ namespace AdventOfCode2018.Core
 
             return twos * threes;
         }
+
+        public static string Process_Part2(string[] ids)
+        {
+            string similarId = null;
+            var idIndex = 0;
+            while (similarId == null && idIndex < ids.Length - 1)
+            {
+                var id = ids[idIndex];
+                var nextIdIndex = idIndex + 1;
+                while (similarId == null && nextIdIndex < ids.Length)
+                {
+                    var compareId = ids[nextIdIndex];
+                    var incorrectCharIndexes = new List<int>();
+
+                    var nextCharIndex = 0;
+                    while (incorrectCharIndexes.Count < 2 && nextCharIndex < id.Length)
+                    {
+                        var match = id[nextCharIndex] == compareId[nextCharIndex];
+                        if (!match)
+                            incorrectCharIndexes.Add(nextCharIndex);
+                        nextCharIndex++;
+                    }
+
+                    if (incorrectCharIndexes.Count == 1)
+                        similarId = string.Join(string.Empty,
+                            id.Where((x, index) => !incorrectCharIndexes.Contains(index)));
+                    nextIdIndex++;
+                }
+                idIndex++;
+            }
+
+            return similarId;
+        }
+
+        //  First Idea
+        //public static string Process_Part2(string[] ids)
+        //{
+        //    var similarIds = new List<string>();
+        //    for (var i1 = 0; i1 < ids.Length - 1; i1++)
+        //    {
+        //        var id = ids[i1];
+
+        //        for (var i2 = i1 + 1; i2 < ids.Length; i2++)
+        //        {
+        //            var compareId = ids[i2];
+        //            var incorrectCharIndexes = new List<int>();
+        //            for (var idI = 0; idI < id.Length; idI++)
+        //            {
+        //                var match = id[idI] == compareId[idI];
+        //                if (!match)
+        //                    incorrectCharIndexes.Add(idI);
+        //            }
+
+        //            if (incorrectCharIndexes.Count == 1)
+        //            {
+        //                similarIds.Add(string.Join(string.Empty, id.Where((x, index) => !incorrectCharIndexes.Contains(index))));
+        //            }
+        //        }
+        //    }
+
+        //    return similarIds.Single();
+        //}
     }
 }
--- src/AdventOfCode2018.Tests/Day02_InventoryManagementSystemTests.cs
+++ src/AdventOfCode2018.Tests/Day02_InventoryManagementSystemTests.cs
@@ -8,21 +8,39 @@ namespace AdventOfCode2018.Tests
     public class Day02_InventoryManagementSystemTests
     {
         [TestMethod]
-        public void Day02_InventoryManagementSystemTests_Test()
+        public void Day02_InventoryManagementSystemTests_Part1_Test()
         {
-            var inputs = new string[] { "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab" };
+            var inputs = new[] { "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab" };
 
-            var result = Day02_InventoryManagementSystem.Process(inputs);
+            var result = Day02_InventoryManagementSystem.Process_Part1(inputs);
             result.ShouldBe(12);
         }
 
         [TestMethod]
-        public void Day02_InventoryManagementSystemTests_Answer()
+        public void Day02_InventoryManagementSystemTests_Part1_Answer()
         {
             var ids = TestResourceService.GetFileContentsByFile("Day02.txt");
 
-            var result = Day02_InventoryManagementSystem.Process(ids);
+            var result = Day02_InventoryManagementSystem.Process_Part1(ids);
             result.ShouldBe(0);
+        }
+
+        [TestMethod]
+        public void Day02_InventoryManagementSystemTests_Part2_Test()
+        {
+            var inputs = new[] { "abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz" };
+
+            var result = Day02_InventoryManagementSystem.Process_Part2(inputs);
+            result.ShouldBe("fgij");
+        }
+
+        [TestMethod]
+        public void Day02_InventoryManagementSystemTests_Part2_Answer()
+        {
+            var ids = TestResourceService.GetFileContentsByFile("Day02.txt");
+
+            var result = Day02_InventoryManagementSystem.Process_Part2(ids);
+            result.ShouldBe("");
         }
     }
 }
