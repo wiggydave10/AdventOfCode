9bf415941c12040aefc41304594be15c205e42c8
Matthew Yates <matthewy10@hotmail.com>
2018-12-09 16:10:49 +0000

    Completed Day 3 Part 1

---
+++ src/AdventOfCode2018.Tests/Day03_NoMatterHowYouSliceIt.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode2018.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace AdventOfCode2018.Tests
+{
+    [TestClass]
+    public class Day03_NoMatterHowYouSliceItTests
+    {
+        [TestMethod]
+        public void Day03_NoMatterHowYouSliceIt_Part1_Test()
+        {
+            var testData = new[] { "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2" };
+
+            var result = Day03_NoMatterHowYouSliceIt.Process_Part1(testData);
+            result.ShouldBe(4);
+        }
+        [TestMethod]
+        public void Day03_NoMatterHowYouSliceIt_Part1_Answer()
+        {
+            var testData = TestResourceService.GetFileContentsByFile("Day03.txt");
+
+            var result = Day03_NoMatterHowYouSliceIt.Process_Part1(testData);
+            result.ShouldBe(0);
+        }
+    }
+}
