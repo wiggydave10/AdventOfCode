7a78b91d20ea2fa6f3ae6a5c491a84712363bb5a
Matthew Yates <matthewy10@hotmail.com>
2019-05-26 21:26:55 +0100

    Completed Day 6 Part 1

--- src/AdventOfCode2018.Core/Day03_NoMatterHowYouSliceIt.cs
+++ src/AdventOfCode2018.Core/Day03_NoMatterHowYouSliceIt.cs
@@ -115,48 +115,5 @@ namespace AdventOfCode2018.Core
         {
             return Id.GetHashCode() ^ Width.GetHashCode() ^ Height.GetHashCode() ^ Coordinates.GetHashCode();
         }
-    }
-
-    internal struct Coordinate
-    {
-        public Coordinate(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public int X { get; }
-        public int Y { get; }
-
-
-        public override string ToString()
-        {
-            return $"X: {X}, Y: {Y}";
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Coordinate coord && Equals(coord);
-        }
-
-        public bool Equals(Coordinate coord)
-        {
-            return X == coord.X && Y == coord.Y;
-        }
-
-        public override int GetHashCode()
-        {
-            return X.GetHashCode() ^ Y.GetHashCode();
-        }
-    }
-
-    internal static class CoordinateExtensions
-    {
-        public static IEnumerable<Coordinate> GetOccupiedCoordinates(this Coordinate coordinate, int width, int height)
-        {
-            for (var yRow = 0; yRow < height; yRow++)
-                for (var xRow = 0; xRow < width; xRow++)
-                    yield return new Coordinate(coordinate.X + xRow, coordinate.Y + yRow);
-        }
-    }
+    }    
 }
---
+++ src/AdventOfCode2018.Tests/Day06_ChronalCoordinates.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode2018.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018.Tests
+{
+    [TestClass]
+    public class Day06_ChronalCoordinatesTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(Part1_TestData))]
+        public void Day06_ChronalCoordinatesTests_Part1_Test(string[] coordinates, int mostClaimedSpace)
+        {
+            var maxSpace = Day06_ChronalCoordinates.Process_Part1(coordinates);
+            maxSpace.ShouldBe(mostClaimedSpace);
+        }
+
+        [TestMethod]
+        public void Day06_ChronalCoordinatesTests_Part1_Answer()
+        {
+            var testData = TestResourceService.GetFileContentsByFile("Day06.txt");
+
+            var maxSpace = Day06_ChronalCoordinates.Process_Part1(testData);
+            maxSpace.ShouldBe(0);
+        }
+
+
+        public static IEnumerable<object[]> Part1_TestData => new List<object[]>
+        {
+            new object[] { new string[] { "1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9" }, 17 }
+        };
+    }
+}
