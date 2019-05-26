9d709a805af914f66fe9df9309d83632453f0089
Matthew Yates <matthewy10@hotmail.com>
2019-05-26 12:44:28 +0100

    Completed Day 5 Part 1

---
+++ src/AdventOfCode2018.Core/Day05_AlchemicalReduction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018.Core
+{
+    public class Day05_AlchemicalReduction
+    {
+        public static int Process_Part1(string entry)
+        {
+            var characters = new List<char>();
+            characters.AddRange(entry);
+
+            var index = 0;
+            while(index < characters.Count - 1)
+            {
+                var chars = characters.GetRange(index, 2);
+                var char1 = chars[0];
+                var char2 = chars[1];
+
+                var reactive = false;
+                if (char.IsUpper(char1) && (char1 + 32) == char2)
+                {
+                    reactive = true;
+                }
+                else if (char.IsLower(char1) && (char1 - 32) == char2)
+                {
+                    reactive = true;
+                }
+
+                if (reactive)
+                {
+                    characters.RemoveRange(index, 2);
+                    index = index == 0 ? 0 : index - 1;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            return characters.Count;
+        }
+    }
+}
---
+++ src/AdventOfCode2018.Tests/Day05_AlchemicalReductionTests.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode2018.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018.Tests
+{
+    [TestClass]
+    public class Day05_AlchemicalReductionTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(Part1_TestData))]
+        public void Day05_AlchemicalReductionTests_Part1_Test(string polymer, int expectedChainLength)
+        {
+            var chainResult = Day05_AlchemicalReduction.Process_Part1(polymer);
+            chainResult.ShouldBe(expectedChainLength);
+        }
+        [TestMethod]
+        public void Day05_AlchemicalReductionTests_Part1_Answer()
+        {
+            var testData = TestResourceService.GetFileContentsByFile("Day05.txt");
+
+            var result = Day05_AlchemicalReduction.Process_Part1(testData[0]);
+            result.ShouldBe(0);
+        }
+
+
+
+        public static IEnumerable<object[]> Part1_TestData => new List<object[]>
+        {
+            new object[] { "aA", 0 },
+            new object[] { "abBA", 0 },
+            new object[] { "abAB", 4 },
+            new object[] { "aabAAB", 6 },
+            new object[] { "dabAcCaCBAcCcaDA", 10 },
+        };
+    }
+}
