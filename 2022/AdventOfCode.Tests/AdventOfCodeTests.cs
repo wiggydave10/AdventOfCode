415c407ca1c33522caaa7cc5204f9f6449a2eb4e
Matthew Yates <matthewy10@hotmail.com>
2022-12-04 15:42:15 +0000

    Added 2nd part (evening) of Day 01

--- 2022/AdventOfCode.Tests/AdventOfCodeTests.cs
+++ 2022/AdventOfCode.Tests/AdventOfCodeTests.cs
@@ -3,11 +3,19 @@ namespace AdventOfCode.Tests;
 public class AdventOfCodeTests
 {
 	[Fact]
-	public async Task Day01()
+	public async Task Day01_Morning()
 	{
 		var day01 = new Day01();
 		await day01.PrepData();
-		var result = day01.RunDay();
+		var result = day01.RunMorning();
+		Assert.Equal(0, result);
+	}
+	[Fact]
+	public async Task Day01_Evening()
+	{
+		var day01 = new Day01();
+		await day01.PrepData();
+		var result = day01.RunEvening();
 		Assert.Equal(0, result);
 	}
 }
--- 2022/AdventOfCode.Tests/TestAdventOfCodeTests.cs
+++ 2022/AdventOfCode.Tests/TestAdventOfCodeTests.cs
@@ -3,11 +3,19 @@ namespace AdventOfCode.Tests;
 public class TestAdventOfCodeTests
 {
     [Fact]
-    public async Task Day01()
+    public async Task Day01_Morning()
     {
         var day01 = new Day01();
         await day01.PrepTestData();
-        var result = day01.RunDay();
+        var result = day01.RunMorning();
         Assert.Equal(24000, result);
     }
+	[Fact]
+	public async Task Day01_Evening()
+	{
+		var day01 = new Day01();
+		await day01.PrepTestData();
+		var result = day01.RunEvening();
+		Assert.Equal(45000, result);
+	}
 }
